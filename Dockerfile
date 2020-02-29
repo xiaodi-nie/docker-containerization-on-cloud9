2229c1b1d31e3b3e76d2f4b7bb167d32f9212e80
EC2 Default User <ec2-user@ip-172-31-32-63.ec2.internal>
2020-02-29 02:02:04 +0000

    change to flask script

--- Dockerfile
+++ Dockerfile
@@ -10,3 +10,8 @@ COPY . app.py /app/
 # hadolint ignore=DL3013
 RUN pip install --upgrade pip &&\
     pip install --trusted-host pypi.python.org -r requirements.txt
+
+EXPOSE 8080
+
+# Run app.py at container launch
+CMD ["python", "app.py"]
--- app.py
+++ app.py
@@ -1,9 +1,42 @@
 #!/usr/bin/env python
-import click
+from flask import Flask
+from flask import jsonify
+from pandas import pandas as pd
 
-@click.command()
+
+# If `entrypoint` is not defined in app.yaml, App Engine will look for an app
+# called `app` in `main.py`.
+app = Flask(__name__)
+
+
+@app.route('/greeting')
 def hello():
-    click.echo('Hello World!')
+    """Return a friendly HTTP greeting."""
+    return 'Hello Fellas continuous deployment on GCP rox yeeeeeheee!'
+
+@app.route('/name/<value>')
+def name(value):
+    val = {"value": value}
+    return jsonify(val)
+
+@app.route('/')
+def html():
+    """Returns some custom HTML"""
+    return """
+    <title>This is a Hello World World Page</title>
+    <h3>Hello</h3>
+    <br>
+    <p><b>This is a simple flask web page</b></p>
+    <p>Wrapped inside a Docker container and running on cloud9</p>
+    """
+
+@app.route('/pandas')
+def pandas_sugar():
+    df = pd.read_csv("https://raw.githubusercontent.com/noahgift/sugar/master/data/education_sugar_cdc_2003.csv")
+    return jsonify(df.to_dict())
 
 if __name__ == '__main__':
-    hello()
+    # This is used when running locally only. When deploying to Google App
+    # Engine, a webserver process such as Gunicorn will serve the app. This
+    # can be configured by adding an `entrypoint` to app.yaml.
+    app.run(host='0.0.0.0', port=8080, debug=True)
